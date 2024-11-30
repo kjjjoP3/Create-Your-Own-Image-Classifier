1f8d62db4b2d5cae6873b73c995d4ade9a5e9ce2
ThanhBT <105102395+kjjjoP3@users.noreply.github.com>
2024-11-30 10:54:38 +0700

    update code

--- model.py
+++ model.py
@@ -1,31 +1,31 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from torchvision import datasets, transforms, models
+from torchvision import models
 from collections import OrderedDict
 
-def Net(backbone='vgg16', hidden_units=2048, dropout=0.3):
+def create_model(architecture='vgg16', hidden_units=2048, dropout_rate=0.3):
+    if architecture == 'vgg16':
+        base_model = models.vgg16(pretrained=True)
+    elif architecture == 'densenet121':
+        base_model = models.densenet121(pretrained=True)
+    else:
+        raise ValueError("Model architecture not supported!")
 
-  if backbone == 'vgg16':
-    model = models.vgg16(pretrained=True)
-  elif backbone == 'densenet121':
-    model = models.densenet121(pretrained=True)
-  else:
-    raise ValueError("Unsupported backbone model")
+    # Freeze feature extractor parameters
+    for param in base_model.parameters():
+        param.requires_grad = False
 
-  for param in model.parameters():
-    param.requires_grad = False
-  model.classifier = nn.Sequential(OrderedDict([
-                          ('fc1', nn.Linear(model.classifier[0].in_features, hidden_units)),
-                          ('relu', nn.ReLU()),
-                          ('d_out1', nn.Dropout(p=dropout)),
-                          ('fc2', nn.Linear(hidden_units, 256)),
-                          ('d_out2', nn.Dropout(p=dropout)),
-                          ('relu', nn.ReLU()),
-                          ('fc3', nn.Linear(256, 102)),
-                          ('output', nn.LogSoftmax(dim=1))
-                        ]))
-  return model
+    input_features = base_model.classifier[0].in_features
+    base_model.classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(input_features, hidden_units)),
+        ('relu1', nn.ReLU()),
+        ('dropout1', nn.Dropout(p=dropout_rate)),
+        ('fc2', nn.Linear(hidden_units, 256)),
+        ('relu2', nn.ReLU()),
+        ('dropout2', nn.Dropout(p=dropout_rate)),
+        ('fc3', nn.Linear(256, 102)),
+        ('output', nn.LogSoftmax(dim=1))
+    ]))
+
+    return base_model
+el
